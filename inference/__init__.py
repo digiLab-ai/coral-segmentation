1e36ef64830b18b34fca0d3e11cc4e8d5fbd3593
Sai Aakash <saiaakash60@gmail.com>
2024-11-06 14:09:35 +0000

    refactored inference code

---
+++ inference/__init__.py
@@ -0,0 +1,3 @@
+from .visualizer import ColorMode, GenericMask, _PanopticPrediction, VisImage, Visualizer
+from .coralscop_inference import run_coralscop
+from .coralscop_visualization import visualize_predictions
