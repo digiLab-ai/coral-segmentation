a3b4e607817950be30fc9c7aa6bafc948e6c311d
SaiAakash <saiaakash60@gmail.com>
2025-04-07 11:43:00 +0100

    Black formatted pre processing script

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,13 @@ import os
 import argparse
 import cv2
 
-def get_frames_from_video(video_path: str, img_path: str, sampling_rate: int = 10, downsampling_factor: float = 0.3) -> None:
+
+def get_frames_from_video(
+    video_path: str,
+    img_path: str,
+    sampling_rate: int = 10,
+    downsampling_factor: float = 0.3,
+) -> None:
     """
     Function to pre-process and to obtain a set of frames from the video
 
@@ -44,7 +50,9 @@ def get_frames_from_video(video_path: str, img_path: str, sampling_rate: int = 1
         if success:
             if frameNr < n_frames:
                 if frameNr % sampling_rate == 0:
-                    resized_frame = cv2.resize(frame, (0, 0), fx = downsampling_factor, fy = downsampling_factor)
+                    resized_frame = cv2.resize(
+                        frame, (0, 0), fx=downsampling_factor, fy=downsampling_factor
+                    )
                     cv2.imwrite(
                         os.path.join(img_path, f"frame_{frameNr}.jpg"), resized_frame
                     )
@@ -55,11 +63,18 @@ def get_frames_from_video(video_path: str, img_path: str, sampling_rate: int = 1
 
     capture.release()
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--video_path", type=str, help="Input Video Path")
-parser.add_argument("--img_path", type=str, default="imgs", help="Output path to store images")
-parser.add_argument("--sampling_rate", type=int, default=10, help="Temporal Sampling Rate")
-parser.add_argument("--downsampling_factor", type=float, default=0.3, help="Spatial Downsampling Factor")
+parser.add_argument(
+    "--img_path", type=str, default="imgs", help="Output path to store images"
+)
+parser.add_argument(
+    "--sampling_rate", type=int, default=10, help="Temporal Sampling Rate"
+)
+parser.add_argument(
+    "--downsampling_factor", type=float, default=0.3, help="Spatial Downsampling Factor"
+)
 
 args = parser.parse_args()
 
@@ -68,7 +83,9 @@ img_path = args.img_path
 sampling_rate = args.sampling_rate
 downsampling_factor = args.downsampling_factor
 
-get_frames_from_video(video_path=video_path, img_path=img_path, sampling_rate=sampling_rate, downsampling_factor=downsampling_factor)
-
-
-
+get_frames_from_video(
+    video_path=video_path,
+    img_path=img_path,
+    sampling_rate=sampling_rate,
+    downsampling_factor=downsampling_factor,
+)
